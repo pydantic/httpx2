f3f8e04af0181c036aa7e732b70990c579bd4877
Hood Chatham <roberthoodchatham@gmail.com>
2026-08-05 15:21:25 -0700

    Use httpx2_jsfetch on Emscripten

--- src/httpx2/httpx2/_transports/__init__.py
+++ src/httpx2/httpx2/_transports/__init__.py
@@ -1,8 +1,23 @@
+import sys
+
 from .asgi import ASGITransport
 from .base import AsyncBaseTransport, BaseTransport
-from .default import AsyncHTTPTransport, HTTPTransport
 from .mock import MockTransport
 from .wsgi import WSGITransport
+
+if sys.platform == "emscripten":  # pragma: nocover
+    # in emscripten we use javascript fetch
+    from httpx2_jsfetch import (
+        AsyncJavascriptFetchTransport,
+        JavascriptFetchTransport,
+    )
+
+    # override default transport names
+    HTTPTransport = JavascriptFetchTransport
+    AsyncHTTPTransport = AsyncJavascriptFetchTransport
+else:
+    # everywhere else we use default
+    from .default import AsyncHTTPTransport, HTTPTransport
 
 __all__ = [
     "ASGITransport",
