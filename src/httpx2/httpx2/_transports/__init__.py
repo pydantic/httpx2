d6af39eed87f32c26ab6d7d15cd48b05554003a9
Hood Chatham <roberthoodchatham@gmail.com>
2026-08-07 08:46:00 -0700

    Fix transport permutation

--- src/httpx2/httpx2/_transports/__init__.py
+++ src/httpx2/httpx2/_transports/__init__.py
@@ -13,8 +13,8 @@ if sys.platform == "emscripten":  # pragma: nocover
 
     # in emscripten we use javascript fetch
     from httpx2_jsfetch import (
-        AsyncJavascriptFetchTransport as HTTPTransport,
-        JavascriptFetchTransport as AsyncHTTPTransport,
+        JavascriptFetchTransport as HTTPTransport,
+        AsyncJavascriptFetchTransport as AsyncHTTPTransport,
     )
 else:
     # everywhere else we use default
