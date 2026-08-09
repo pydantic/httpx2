4dcdf3457d9e5c51ea6fd7ed75103b5799a98239
Marcelo Trylesinski <marcelotryle@gmail.com>
2026-08-09 10:03:57 +0200

    Avoid quadratic copying when sending large HTTP/2 request bodies

--- src/httpcore2/httpcore2/_sync/http2.py
+++ src/httpcore2/httpcore2/_sync/http2.py
@@ -246,10 +246,11 @@ class HTTP2Connection(ConnectionInterface):
         """
         Send a single chunk of data in one or more data frames.
         """
-        while data:
+        position = 0
+        while position < len(data):
             max_flow = self._wait_for_outgoing_flow(request, stream_id)
-            chunk_size = min(len(data), max_flow)
-            chunk, data = data[:chunk_size], data[chunk_size:]
+            chunk = data[position : position + max_flow]
+            position += len(chunk)
             self._h2_state.send_data(stream_id, chunk)
             self._write_outgoing_data(request)
 
