ec1e8094b6398d246a0e8d2b2567b29c794dcb43
Marcelo Trylesinski <marcelotryle@gmail.com>
2026-09-03 13:30:45 +0300

    Keep SSL verification help text

--- src/httpx2/httpx2/_main.py
+++ src/httpx2/httpx2/_main.py
@@ -77,7 +77,7 @@ def print_help() -> None:
     )
 
     table.add_row("--follow-redirects", "Automatically follow redirects.")
-    table.add_row("--verify / --no-verify", "Enable or disable SSL verification.")
+    table.add_row("--verify / --no-verify", "Enable SSL verification.")
     table.add_row("--http2", "Send the request using HTTP/2, if the remote server supports it.")
 
     table.add_row(
@@ -395,7 +395,7 @@ def handle_help(
 @click.option(
     "--verify/--no-verify",
     default=True,
-    help="Enable or disable SSL verification.",
+    help="Enable SSL verification.",
 )
 @click.option(
     "--http2",
