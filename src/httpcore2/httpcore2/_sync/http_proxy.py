19da243ccc69342c07a1e0d0134a301519766daa
Marcelo Trylesinski <marcelotryle@gmail.com>
2026-09-22 17:07:15 +0200

    Name the resolved proxy tunnel TLS hostname

--- src/httpcore2/httpcore2/_sync/http_proxy.py
+++ src/httpcore2/httpcore2/_sync/http_proxy.py
@@ -292,10 +292,10 @@ class TunnelHTTPConnection(ConnectionInterface):
                 alpn_protocols = ["http/1.1", "h2"] if self._http2 else ["http/1.1"]
                 ssl_context.set_alpn_protocols(alpn_protocols)
 
+                server_hostname = request.extensions.get("sni_hostname") or self._remote_origin.host.decode("ascii")
                 kwargs = {
                     "ssl_context": ssl_context,
-                    "server_hostname": request.extensions.get("sni_hostname")
-                    or self._remote_origin.host.decode("ascii"),
+                    "server_hostname": server_hostname,
                     "timeout": timeout,
                 }
                 with Trace("start_tls", logger, request, kwargs) as trace:
